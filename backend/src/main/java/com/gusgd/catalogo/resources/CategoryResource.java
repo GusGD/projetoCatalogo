538a018bfcc7559e52374316521c6de2609b9e16
GusGD <si_gustavo@outlook.com>
2025-04-14 17:54:41 -0300

    refactored

--- backend/src/main/java/com/gusgd/catalogo/resources/CategoryResource.java
+++ backend/src/main/java/com/gusgd/catalogo/resources/CategoryResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gusgd.catalogo.entities.Category;
+import com.gusgd.catalogo.dto.CategoryDTO;
 import com.gusgd.catalogo.services.CategoryService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,8 +20,8 @@ public class CategoryResource {
   private final CategoryService service;
   
   @GetMapping
-  public ResponseEntity<List<Category>> findAll(){
-      List<Category> list = service.findAll();
+  public ResponseEntity<List<CategoryDTO>> findAll(){
+      List<CategoryDTO> list = service.findAll();
       return ResponseEntity.ok().body(list);
   }
 }
--- backend/src/main/java/com/gusgd/catalogo/services/CategoryService.java
+++ backend/src/main/java/com/gusgd/catalogo/services/CategoryService.java
@@ -1,9 +1,11 @@
 package com.gusgd.catalogo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.gusgd.catalogo.dto.CategoryDTO;
 import com.gusgd.catalogo.entities.Category;
 import com.gusgd.catalogo.repositories.CategoryRepository;
 
@@ -12,11 +14,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class CategoryService {
-    
-    private final CategoryRepository repository;
+	
+	private final CategoryRepository repository;
 
-    public List<Category> findAll(){
-        return repository.findAll();
-    }
-    
+	public List<CategoryDTO> findAll(){
+			List<Category> list = repository.findAll();
+			return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
 }
